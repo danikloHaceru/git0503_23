5b9c4e68a6448add040ba65dec50c882d3ef51c2
danikloHaceru <51043439+danikloHaceru@users.noreply.github.com>
2023-03-05 18:58:23 +0200

    Update gitTest.java

--- src/test/gitTest.java
+++ src/test/gitTest.java
@@ -4,7 +4,7 @@ public class gitTest {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             //
-            //a
+            //a +b + c
             System.out.println(i);
         }
     }
